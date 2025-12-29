f5e94febb7ddcf36a5de0f3f36e7756e5b472a86
lexuan <103698@siswa.unimas.my>
2025-12-30 00:32:56 +0800

    user

--- classes/user.py
+++ classes/user.py
@@ -1,26 +1,29 @@
 class User:
-    def __init__(self, user_id, user_name, user_email, password):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.user_email = user_email
-        self.password = password
-        self.is_verified = False
+    def __init__(self, user_id, user_name, user_email, user_password):
+        self.userID = user_id
+        self.userName = user_name
+        self.userEmail = user_email
+        self.userPassword = user_password
+        self.userVerificationStatus = False
 
     def register(self):
         print("User registered successfully.")
 
-    def verify_email(self):
-        self.is_verified = True
-        print("Email verified.")
-
-    def reset_password(self, new_password):
-        self.password = new_password
-        print("Password reset successful.")
+    def login(self):
+        print("User logged in.")
 
     def logout(self):
         print("User logged out.")
 
-    def view_profile(self):
+    def viewProfile(self):
         print("User Profile")
-        print("Name:", self.user_name)
-        print("Email:", self.user_email)
+        print("Name:", self.userName)
+        print("Email:", self.userEmail)
+
+    def verifyEmail(self):
+        self.userVerificationStatus = True
+        print("Email verified.")
+
+    def resetPassword(self, new_password):
+        self.userPassword = new_password
+        print("Password reset successful.")
